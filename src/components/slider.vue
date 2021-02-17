9d5dd4f2e157527d0542e5d521d07ea3650c4888
Youssef <yousoufos@gmail.com>
2021-02-17 10:18:07 +0100

    554511

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,26 +1,79 @@
 <template>
-    <div
-        class="mx-auto flex flex-nowrap w-4/5 lg:10/12 space-x-2 overflow-x-auto"
-    >
-        <div
-            v-for="item in pictures"
-            class="w-52 mx-auto rounded-md text-white flex-none flex items-center justify-center text-2xl font-semibold"
+    <div>
+        <Carousel
+            @next="next"
+            @previous="previous"
+            @show="showPicture"
+            :slides="slides"
+            :selectedPicture="selectedPicture"
         >
-            <img :src="item" alt="" />
-        </div>
+            <CarouselSlide
+                v-for="(slide, index) in slides"
+                :key="index"
+                :index="index"
+                :visibleSlide="visibleSlide"
+                :direction="direction"
+                @picture="activePicture"
+                ><img :src="slide"
+            /></CarouselSlide>
+        </Carousel>
     </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import Carousel from '../components/Carousel'
+import CarouselSlide from '../components/CarouselSlide'
 export default {
-    props: {
-        pictures: Array,
-    },
+    components: { Carousel, CarouselSlide },
     setup() {
-        const tabImg = ['ps5.jpg', 'iphone12.jpg', 'ps5.jpg', 'ps5.jpg']
+        const slides = ref([
+            'https://picsum.photos/id/237/600/350',
+            'https://picsum.photos/id/238/600/350',
+            'https://picsum.photos/id/239/600/350',
+            'https://picsum.photos/id/230/600/350',
+            'https://picsum.photos/id/231/600/350',
+            'https://picsum.photos/id/233/600/350',
+            'https://picsum.photos/id/234/600/350',
+            'https://picsum.photos/id/236/600/350',
+        ])
+        const direction = ref('left')
+        const visibleSlide = ref(0)
+        const selectedPicture = ref(0)
+        const slidesLen = computed(() => {
+            return slides.value.length
+        })
+        const previous = (params) => {
+            if (visibleSlide.value <= 0) {
+                visibleSlide.value = slidesLen.value - 1
+            } else {
+                visibleSlide.value--
+            }
+            direction.value = 'right'
+        }
+        const next = (params) => {
+            if (visibleSlide.value >= slidesLen.value - 1) {
+                visibleSlide.value = 0
+            } else {
+                visibleSlide.value++
+            }
+            direction.value = 'left'
+        }
+        const activePicture = (params) => {
+            selectedPicture.value = params
+        }
+        const showPicture = (params) => {
+            visibleSlide.value = params
+        }
         return {
-            tabImg,
+            slides,
+            visibleSlide,
+            previous,
+            next,
+            direction,
+            activePicture,
+            selectedPicture,
+            showPicture,
         }
     },
 }
---
+++ src/components/slider1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        class="mx-auto flex flex-nowrap w-4/5 lg:10/12 space-x-2 overflow-x-auto"
+    >
+        <div
+            v-for="item in pictures"
+            class="w-52 mx-auto rounded-md text-white flex-none flex items-center justify-center text-2xl font-semibold"
+        >
+            <img :src="item" alt="" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    props: {
+        pictures: Array,
+    },
+    setup() {
+        const tabImg = ['ps5.jpg', 'iphone12.jpg', 'ps5.jpg', 'ps5.jpg']
+        return {
+            tabImg,
+        }
+    },
+}
+</script>
+
+<style></style>
